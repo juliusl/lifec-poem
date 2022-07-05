704d7dffa533c5207f84dc4df8d9e15547190a18
Julius Liu <juliusl@microsoft.com>
2022-07-05 13:15:09 -0700

    Feat. adding static-file server plugin

---
+++ src/static_files.rs
@@ -0,0 +1,58 @@
+
+use lifec::plugins::{Plugin, ThunkContext};
+use poem::{Route, Server, endpoint::StaticFilesEndpoint, listener::TcpListener};
+use tokio::{select, sync::oneshot::Sender, task::JoinHandle};
+
+/// Static files plugin that starts a server host on text_attribute `address`
+/// and serving files from `work_dir`.
+pub struct StaticFiles;
+
+impl Plugin<ThunkContext> for StaticFiles {
+    fn symbol() -> &'static str {
+        "static_files"
+    }
+
+    fn description() -> &'static str {
+        "Starts a static file server host for file directory specified by `work_dir`"
+    }
+
+    fn call_with_context(context: &mut ThunkContext) -> Option<(JoinHandle<ThunkContext>, Sender<()>)> {
+        context.clone().task(|cancel_source| {
+            let tc = context.clone();
+            async {
+                if let Some(work_dir) = tc.as_ref().find_text("work_dir") {
+                    tc.update_status_only(format!("Serving work_dir {}", work_dir)).await;
+                    let app = Route::new().nest(
+                        "/",
+                        StaticFilesEndpoint::new(
+                            work_dir
+                        ),
+                    );
+
+                    if let Some(address) = tc.as_ref().find_text("address") {
+                        tc.update_status_only(format!("Starting {}", address)).await;
+                        select! {
+                            result = Server::new(
+                                TcpListener::bind(address))
+                                .run(app) => {
+                                    match result {
+                                        Ok(_) => {
+                                            tc.update_status_only("Server is exiting").await; 
+                                        },
+                                        Err(err) => {
+                                            tc.update_status_only(format!("Server error exit {}", err)).await;
+                                        },
+                                    }
+                            }
+                            _ = cancel_source => {
+                                tc.update_status_only("Cancelling, server is exiting").await; 
+                            }
+                        }
+                    }
+                }
+
+                Some(tc)
+            }
+        })
+    }
+}
