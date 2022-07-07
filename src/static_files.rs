6874e1b226e4534f191f4c0035096386cc8b4eb0
Julius Liu <juliusl@microsoft.com>
2022-07-07 16:33:43 -0700

    feat. host single file

--- src/static_files.rs
+++ src/static_files.rs
@@ -1,5 +1,7 @@
+use std::path::PathBuf;
+
 use lifec::{plugins::{Plugin, ThunkContext, AsyncContext}, Component, DenseVecStorage};
-use poem::{Route, endpoint::StaticFilesEndpoint};
+use poem::{Route, endpoint::{StaticFilesEndpoint, StaticFileEndpoint}};
 use crate::{WebApp, AppHost};
 
 
@@ -35,21 +37,22 @@ impl WebApp for StaticFiles {
 
         let path_prefix = format!("/{block_name}");
         eprintln!("{}", path_prefix);
-        Route::new().nest(
+
+        if let Some(index_html) = index_html {
+            eprintln!("setting index - {}", index_html);
+
+            let file_src = PathBuf::from(&work_dir).join(index_html);
+
+            Route::new().nest(path_prefix, StaticFileEndpoint::new(file_src))
+        } else {
+            Route::new().nest(
                 path_prefix,
-            {
-                let mut static_files = StaticFilesEndpoint::new(
+
+                StaticFilesEndpoint::new(
                     work_dir.to_string()
-                );
-
-                if let Some(index_html) = index_html {
-                    eprintln!("setting index - {}", index_html);
-                    static_files = static_files.index_file(index_html.to_string());
-                }
-
-                static_files
-            }
-        )
+                )
+            )
+        }
     }
 }
 
