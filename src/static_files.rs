e1cb365a661a96be36a5e782dd9168ad59688857
Julius Liu <juliusl@microsoft.com>
2022-07-07 16:56:08 -0700

    revert

--- src/static_files.rs
+++ src/static_files.rs
@@ -37,24 +37,20 @@ impl WebApp for StaticFiles {
 
         let path_prefix = format!("/{block_name}");
         eprintln!("{}", path_prefix);
-        let mut routes = Route::new().nest(
+        Route::new().nest(
                 &path_prefix,
             {
-                let static_files = StaticFilesEndpoint::new(
+                let mut static_files = StaticFilesEndpoint::new(
                     work_dir.to_string()
-                );
+                );        
+
+                if let Some(index_html) = index_html {
+                    static_files =  static_files.index_file(index_html.to_string());
+                }
 
                 static_files
             }
-        );
-        
-        
-        if let Some(index_html) = index_html {
-            let index_html = PathBuf::from(&work_dir).join(index_html);
-            routes = routes.at("/", StaticFileEndpoint::new(index_html))
-        }
-     
-        routes 
+        )
     }
 }
 
